ad55293db1e6ec077e2409a9c286ed54e44a0301
Maks <maxkov19861411@gmail.com>
2024-10-20 18:16:58 +0300

    Create next_js_app

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import styles from "./page.module.css";
+import course from "./course.json";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <ul>
+          {course.lessons.map((lesson) => (
+            <li key={lesson.name}>
+              <h2>{lesson.title}</h2>
+              <p>{lesson.points}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
